34c0ce0fa93b5ad3bc81aacd58a5fbfe416fff2f
Dejan Dimic <dejan.dimic@gmail.com>
2024-01-09 22:10:26 +0100

    feat: expand the middle text selection
    Use two different approaches to alter component config

--- layouts/docs.vue
+++ layouts/docs.vue
@@ -6,7 +6,7 @@ const navigation = inject<Ref<NavItem[]>>('navigation')
 
 <template>
   <UContainer>
-    <UPage>
+    <UPage :ui="{ wrapper: 'flex flex-col lg:grid lg:grid-cols-12 lg:gap-8', center: { narrow: 'lg:col-span-10', base: 'lg:col-span-10', full: 'lg:col-span-12' } }">
       <template #left>
         <UAside>
           <UNavigationTree :links="mapContentNavigation(navigation)" />
