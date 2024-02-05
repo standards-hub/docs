241b49d59e31313cc4abf627c8e85992efa7eef6
Filip <dr.ipi95@gmail.com>
2024-02-05 23:23:52 +0100

    update: breadcrumbs now showing folder path and file path

--- components/ShBreadcrumbs.vue
+++ components/ShBreadcrumbs.vue
@@ -2,29 +2,31 @@
   <header class="h-8 w-full bg-zinc-200 dark:bg-slate-800">
     <nuxt-link to="/">Home</nuxt-link>
     <span v-for="(crumb, index) in breadcrumbs" :key="index">
-      /
-      <nuxt-link :to="crumb.to" v-if="crumb.label">{{ crumb.label }}</nuxt-link>
+      >
+      <template v-if="crumb.to">
+        <nuxt-link :to="crumb.to">{{ crumb.label }}</nuxt-link>
+      </template>
+      <span v-else>{{ crumb.label }}</span>
     </span>
   </header>
 </template>
 
-<script >
+<script>
 export default {
   computed: {
     breadcrumbs() {
       const pathSegments = this.$route.path.split('/').filter(segment => segment !== '');
-      return pathSegments.map((segment, index) => ({
-        to: `/${pathSegments.slice(0, index + 1).join('/')}`,
-        label: index === pathSegments.length - 1 ? this.capitalize(segment) : '',
-      })).filter(crumb => crumb.label !== '');
+      return pathSegments.map((segment, index) => {
+        const isLast = index === pathSegments.length - 1;
+        const label = isLast ? this.capitalize(segment) : segment;
+        const to = isLast ? `/${pathSegments.slice(0, index + 1).join('/')}` : null;
+        return { to, label };
+      });
     },
   },
   methods: {
     capitalize(str) {
       return str.charAt(0).toUpperCase() + str.slice(1);
-    },
-    shouldShowSlash(index) {
-      return index > 0 && this.breadcrumbs[index].label !== '';
     },
   },
 };
