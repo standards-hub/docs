63371953b57a071af532b13f52a034b890b639e0
Filip <dr.ipi95@gmail.com>
2024-02-05 22:45:19 +0100

    feat: added working breadcrumbs

--- components/ShBreadcrumbs.vue
+++ components/ShBreadcrumbs.vue
@@ -1,38 +1,31 @@
 <template>
   <header class="h-8 w-full bg-zinc-200 dark:bg-slate-800">
-    <UBreadcrumb divider=">" class="flex mx-5 -bottom-1.5 left-0">
-      <template #default="{ link, isActive, index }">
-        <UBadge :color="isActive ? 'primary' : 'gray'" class="rounded-full">
-          {{ index + 1 }}. {{ link.label }}
-        </UBadge>
-      </template>
-    </UBreadcrumb>
+    <nuxt-link to="/">Home</nuxt-link>
+    <span v-for="(crumb, index) in breadcrumbs" :key="index">
+      /
+      <nuxt-link :to="crumb.to" v-if="crumb.label">{{ crumb.label }}</nuxt-link>
+    </span>
   </header>
 </template>
 
-<script setup>
-import { ref, onMounted } from "vue";
-
-// Task 1: Get the current full route
-const currentFullRoute = ref("");
-
-// Task 2: Create an empty array called bread
-const bread = ref([]);
-
-// Task 3: Insert full path into the array bread
-onMounted(() => {
-  currentFullRoute.value = $route.fullPath;
-  bread.value.push(currentFullRoute.value);
-});
-
-// Task 4: Make a function called knife to slice the full route path
-const knife = () => {
-  return currentFullRoute.value.split("/");
+<script >
+export default {
+  computed: {
+    breadcrumbs() {
+      const pathSegments = this.$route.path.split('/').filter(segment => segment !== '');
+      return pathSegments.map((segment, index) => ({
+        to: `/${pathSegments.slice(0, index + 1).join('/')}`,
+        label: index === pathSegments.length - 1 ? this.capitalize(segment) : '',
+      })).filter(crumb => crumb.label !== '');
+    },
+  },
+  methods: {
+    capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    },
+    shouldShowSlash(index) {
+      return index > 0 && this.breadcrumbs[index].label !== '';
+    },
+  },
 };
-
-// Task 5: Create an array called crumbs and push values using the knife function
-const crumbs = ref([]);
-onMounted(() => {
-  crumbs.value = knife().map((value) => ({ dynamicProp: value }));
-});
 </script>
