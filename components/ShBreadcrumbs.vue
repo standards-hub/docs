dc01694bca73054637e4292f880cda6dc0607ee9
Filip <dr.ipi95@gmail.com>
2024-02-12 17:41:15 +0100

    update: visual improvements

--- components/ShBreadcrumbs.vue
+++ components/ShBreadcrumbs.vue
@@ -1,10 +1,15 @@
 <template>
-  <header class="h-8 w-full bg-zinc-200 dark:bg-slate-800 text-xs italic flex items-center">
+  <header class="h-8 p-4 w-full bg-zinc-200 dark:bg-slate-800 text-xs italic flex items-center">
     <nuxt-link to="/">HOME</nuxt-link>
     <span v-for="(crumb, index) in breadcrumbs" :key="index">
-      >
+      &nbsp; / &nbsp;
       <template v-if="crumb.to">
-        <nuxt-link :to="crumb.to">{{ crumb.label.toUpperCase() }}</nuxt-link>
+        <nuxt-link
+          :to="crumb.to"
+          :class="{ 'active': isBreadcrumbActive(crumb, index) }"
+        >
+          {{ crumb.label.toUpperCase() }}
+        </nuxt-link>
       </template>
       <span v-else>{{ crumb.label.toUpperCase() }}</span>
     </span>
@@ -28,6 +33,16 @@ export default {
     capitalize(str) {
       return str.charAt(0).toUpperCase() + str.slice(1);
     },
+    isBreadcrumbActive(crumb, index) {
+      const currentPath = `/${this.$route.path.split('/').filter(segment => segment !== '').slice(0, index + 1).join('/')}`;
+      return crumb.to === currentPath;
+    },
   },
 };
 </script>
+
+<style scoped>
+.active {
+  color: blue; /*change the color of active breadcrumb*/
+}
+</style>
