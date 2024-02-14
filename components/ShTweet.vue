685ce09c5a08a77614baada93c0ffadc0e68c390
Filip <dr.ipi95@gmail.com>
2024-02-14 16:59:58 +0100

    feat: added social media post embed

---
+++ components/ShTweet.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex p-4 justify-center">
+    <NuxtTweet
+      class="bg-white dark:bg-white"
+      :id="id"
+      :show-media="true"
+    />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  id: String,
+});
+</script>
