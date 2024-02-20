30f90f9df3ef5b94a9186fae08ea8b72342068de
Filip <dr.ipi95@gmail.com>
2024-02-20 22:40:45 +0100

    fix: changed ShAlert component to resemble same component in V2

--- components/ShAlert.vue
+++ components/ShAlert.vue
@@ -1,41 +1,31 @@
 <template>
-  <div class="mx-0 p-2">
-    <UAlert
-      :title="title"
-      :description="description"
-      :type="type"
-      :icon="getIconAndColor().icon"
-      :color="getIconAndColor().color"
-      variant="soft"
-    />
+  <div :class="alertClasses">
+    <slot></slot>
   </div>
 </template>
 
-<script setup>
-
-const props = defineProps({
-  title: String,
-  description: String,
-  type: {
-    type: String, // Make sure type is expected as a string
-    default: 'info', // Set a default type if needed
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'info',
+      validator: (value) => ['info', 'success', 'warning', 'danger'].includes(value)
+    }
   },
-  color: String
-});
-
-const getIconAndColor = () => {
-  const typeInfo = {
-    info: { icon: "i-heroicons-information-circle", color: "blue" },
-    success: { icon: "i-heroicons-check-circle", color: "emerald" },
-    warning: { icon: "i-heroicons-exclamation-circle", color: "yellow" },
-    danger: { icon: "i-heroicons-x-circle", color: "red" },
-    wrongType: { icon: "i-heroicons-question-mark-circle", color: "white"},
-  };
-
-  if (typeInfo[props.type]) {
-    return { icon: typeInfo[props.type].icon, color: typeInfo[props.type].color };
-  } else {
-    return { icon: typeInfo.wrongType.icon, color: typeInfo.wrongType.color};
+  computed: {
+    alertClasses() {
+      switch (this.type) {
+        case 'success':
+          return 'bg-[#F0FFF4] dark:bg-[#22543D] text-[#2F855A] dark:text-[#9AE6A9] px-4 py-3 relative border-l-4 border-[#2F855A]';
+        case 'warning':
+          return 'bg-[#FFFAF0] dark:bg-[#744210] text-[#C66328] dark:text-[#FBD375] px-4 py-3 relative border-l-4 border-[#B7791F]';
+        case 'danger':
+          return 'bg-[#FFF5F5] dark:bg-[#742A2A] text-[#D03030] dark:text-[#FEB2B2] px-4 py-3 relative border-l-4 border-[#C53030]';
+        default: // info
+          return 'bg-[#EBF8FF] dark:bg-[#2A4365] text-[#63B3ED] dark:text-[#86CDF4] px-4 py-3 relative border-l-4 border-[#2B6CB0]';
+      }
+    }
   }
 };
 </script>
