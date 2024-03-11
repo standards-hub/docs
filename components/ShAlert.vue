87b7c27437bf9af1cc35057c73d40c680064e3a1
Filip Dragović <dr.ipi95@gmail.com>
2024-03-11 18:56:41 +0100

    fix: ShAlert colors changed

--- components/ShAlert.vue
+++ components/ShAlert.vue
@@ -21,25 +21,25 @@ export default {
     alertClasses() {
       switch (this.type) {
         case "success":
-          return "bg-[#F0FFF4] dark:bg-[#22543D] text-[#2F855A] dark:text-[#9AE6A9] px-2 py-3 relative border-l-4 border-[#2F855A]";
+          return "bg-[#F0FFF4] dark:bg-[#22543D] text-[#2F855A] dark:text-[#9AE6B4] px-2 py-3 relative border-l-4 border-[#68D391] dark:border-[#2F855A]";
         case "warning":
-          return "bg-[#FFFAF0] dark:bg-[#744210] text-[#C66328] dark:text-[#FBD375] px-2 py-3 relative border-l-4 border-[#B7791F]";
+          return "bg-[#FFFAF0] dark:bg-[#744210] text-[#C05621] dark:text-[#FBD38D] px-2 py-3 relative border-l-4 border-[#F6AD55] dark:border-[#B7791F]";
         case "danger":
-          return "bg-[#FFF5F5] dark:bg-[#742A2A] text-[#D03030] dark:text-[#FEB2B2] px-2 py-3 relative border-l-4 border-[#C53030]";
+          return "bg-[#FFF5F5] dark:bg-[#742A2A] text-[#C53030] dark:text-[#FEB2B2] px-2 py-3 relative border-l-4 border-[#FC8181] dark:border-[#C53030]";
         default: // info
-          return "bg-[#EBF8FF] dark:bg-[#2A4365] text-[#63B3ED] dark:text-[#86CDF4] px-2 py-3 relative border-l-4 border-[#2B6CB0]";
+          return "bg-[#EBF8FF] dark:bg-[#2A4365] text-[#2B6CB0] dark:text-[#90CDF4] px-2 py-3 relative border-l-4 border-[#63B3ED] dark:border-[#2B6CB0]";
       }
     },
     iconClass() {
       switch (this.type) {
         case "success":
-          return "text-[#2F855A] dark:text-[#9AE6A9] i-heroicons-check-circle";
+          return "text-[#68D391] dark:text-[#68D391] i-heroicons-check-circle";
         case "warning":
-          return "text-[#C66328] dark:text-[#FBD375] i-heroicons-exclamation-circle";
+          return "text-[#F6AD55] dark:text-[#F6AD55] i-heroicons-exclamation-circle";
         case "danger":
-          return "text-[#D03030] dark:text-[#FEB2B2] i-heroicons-x-circle";
+          return "text-[#FC8181] dark:text-[#FC8181] i-heroicons-x-circle";
         default: // info
-          return "text-[#63B3ED] dark:text-[#86CDF4] i-heroicons-information-circle";
+          return "text-[#63B3ED] dark:text-[#63B3ED] i-heroicons-information-circle";
       }
     },
   },
